c6bd06521d493a84dc7cb9ad7e52296e79250b0f
KwabenBerko <kwabenaberko95@gmail.com>
2018-11-25 23:56:26 +0000

    Minor Improvement In Add Note Activity

--- app/src/main/java/com/kwabenaberko/finito/viewmodel/AddNoteViewModel.kt
+++ app/src/main/java/com/kwabenaberko/finito/viewmodel/AddNoteViewModel.kt
@@ -1,17 +1,25 @@
 package com.kwabenaberko.finito.viewmodel
 
+import android.databinding.Bindable
+import android.util.Log
+import com.android.databinding.library.baseAdapters.BR
 import com.kwabenaberko.finito.model.Note
 import com.kwabenaberko.finito.model.repository.NoteRepository
 import javax.inject.Inject
 
 class AddNoteViewModel
 @Inject constructor (private val noteRepository: NoteRepository) : ObservableViewModel() {
+    @get:Bindable
     var newNote = Note(text = "")
-
 
     fun saveNewNote(){
         checkFieldsNotEmpty(newNote)
         noteRepository.saveNote(newNote)
+    }
+
+    fun onNoteTextChanged(){
+        Log.d("ADDNOTEVIEWMODEL", "Text Changed Called!")
+        notifyPropertyChanged(BR.newNote)
     }
 
     private fun checkFieldsNotEmpty(note: Note){
