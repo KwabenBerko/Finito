cb1dcdd0c631864ed5275f8ae75df4e92d2a6396
KwabenBerko <kwabenaberko95@gmail.com>
2018-11-29 10:28:02 +0000

    Persistence with Room WIP 2 - Added Type Converters For The Priority class

---
+++ app/src/main/java/com/kwabenaberko/finito/model/repository/database/Converters.kt
@@ -0,0 +1,16 @@
+package com.kwabenaberko.finito.model.repository.database
+
+import android.arch.persistence.room.TypeConverter
+import com.kwabenaberko.finito.model.Priority
+
+class Converters {
+    @TypeConverter
+    fun priorityEnumToInt(priority: Priority): Int{
+        return priority.ordinal
+    }
+
+    @TypeConverter
+    fun intToPriorityEnum(ordinal: Int): Priority{
+        return Priority.valueOf(ordinal.toString())
+    }
+}
--- app/src/main/java/com/kwabenaberko/finito/model/repository/database/NoteDatabase.kt
+++ app/src/main/java/com/kwabenaberko/finito/model/repository/database/NoteDatabase.kt
@@ -2,9 +2,11 @@ package com.kwabenaberko.finito.model.repository.database
 
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
+import android.arch.persistence.room.TypeConverters
 import com.kwabenaberko.finito.model.Note
 
 @Database(entities = [Note::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class NoteDatabase : RoomDatabase(){
     abstract fun getNoteDao(): NoteDao
 }
