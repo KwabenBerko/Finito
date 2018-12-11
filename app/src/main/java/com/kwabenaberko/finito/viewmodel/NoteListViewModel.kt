dbe937db345faa45f001fd2a5817b2d0e80e1983
KwabenBerko <kwabenaberko95@gmail.com>
2018-12-11 22:45:51 +0000

    UI WIP 5

--- app/src/main/java/com/kwabenaberko/finito/viewmodel/NoteListViewModel.kt
+++ app/src/main/java/com/kwabenaberko/finito/viewmodel/NoteListViewModel.kt
@@ -29,7 +29,7 @@ class NoteListViewModel
 
     init{
         //For Testing Purposes
-        //deleteSavedNotes()
+        deleteSavedNotes()
     }
 
     fun getNoteList():LiveData<List<NoteListItem>> = Transformations.map(noteRepository.findSavedNotes()) { savedNotes ->
